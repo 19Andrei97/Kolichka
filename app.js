a327f8f6360b951743312e60c109f30dfe79f0e1
Andrea <dredre1997@gmail.com>
2021-05-30 02:50:42 +0300

    Fixed Heroku

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const PORT = 3000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('app'))
+  .get('/', (req, res) => res.render('public/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
