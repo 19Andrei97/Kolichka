801a077ed9011e78fba613ca27db08a286a4c6cc
Andrea <dredre1997@gmail.com>
2021-05-30 02:22:35 +0300

    Fixed backend and added Heroku files

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 
-const publicPath = path.join(__dirname, "/../data");
-const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, "/data");
+const port = 3000;
 let app = express();
 let server = http.createServer(app);
 app.use(express.static(publicPath));
