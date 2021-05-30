fbdeb9e21ea2543ff8b95748405c9866280a4a06
Andrea <dredre1997@gmail.com>
2021-05-30 03:02:42 +0300

    Small changes

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const express = require("express");
 const PORT = 3000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('public/index.html'))
+  .use(express.static(path.join(__dirname, '/public')))
+  .get('/', (req, res) => res.render('app'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
